bdc0bff886a261aa6937aa00864c3b4106980644
Aidan Bane <aidanfbane@gmail.com>
2018-07-22 05:36:17 -0700

    (feat) Handle async nature of speechSynth getVoices

--- src/getVoices.js
+++ src/getVoices.js
@@ -1,5 +1,18 @@
+import slice from "lodash-es/slice";
+
 function getVoices() {
-  return "getVoices";
+  return new Promise((resolve, reject) => {
+    // listen for onvoiceschanged event
+    window.speechSynthesis.onvoiceschanged = function onvoiceschangedCallBack() {
+      resolve(slice(window.speechSynthesis.getVoices()));
+    };
+
+    // start request for cpu voices
+    window.speechSynthesis.getVoices();
+
+    // if no voices found in 5 seconds, return error.
+    setTimeout(() => reject(new Error("No tts voices found")), 5000);
+  });
 }
 
 export default getVoices;
